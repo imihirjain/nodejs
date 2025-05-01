26d3542e9e64d4d8ea5336882591d9b4acda9866
imihirjain <jmanav251@gmail.com>
2025-05-01 21:29:40 +0530

    complete all route handling, error handling

--- app.js
+++ app.js
@@ -177,3 +177,25 @@ const lodash = require("lodash");
 //   }
 //   console.log("Directory removed");
 // });
+
+// Even emitter handle custom event
+const EventEmitter = require("events");
+
+const emitter = new EventEmitter();
+
+// Registering an even listener
+emitter.on("test1", () => {
+  console.log("An event occured in test1");
+});
+
+emitter.on("error", (err) => {
+  console.log("Error event:- ", err.message);
+});
+
+try {
+  emitter.emit("test1");
+  emitter.removeListener("test1");
+  emitter.emit("test1");
+} catch (error) {
+  emitter.emit("error", error);
+}
--- server.js
+++ server.js
@@ -1,5 +1,83 @@
 const http = require("http");
 const url = require("url");
+const queryString = require("querystring");
+// handle url encode data
+const server = http.createServer((req, res) => {
+  if (req.method === "POST" && req.url === "/submit") {
+    let data = "";
+
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    req.on("end", () => {
+      const parsedData = queryString.parse(data);
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify ({ message: "Form Data received", parsedData }));
+    });
+  } else {
+    res.writeHead(404, { "content-type": "text/plain" });
+    res.end("Page not found");
+  }
+});
+
+// middleware function for log in request
+// function logRequest(req, res, next) {
+//   console.log(`${req.method} req made to ${req.url}`);
+//   next(req, res);
+// }
+
+// const server = http.createServer((req, res) => {
+//   logRequest(req, res, (req, res) => {
+//     const { pathname } = url.parse(req.url);
+//     if (pathname.startsWith("/user/")) {
+//       const userId = pathname.split("/")[2];
+//       res.writeHead(200, { "content-type": "text/plain" });
+//       res.end(`User id is:- ${userId}`);
+//     } else {
+//       res.writeHead(404, { "content-type": "text/plain" });
+//       res.end("Page not found");
+//     }
+//   });
+// });
+
+// Dynamic route handler
+// const server = http.createServer((req, res) => {
+//   const { pathname } = url.parse(req.url);
+//   if (pathname.startsWith("/user/")) {
+//     const userId = pathname.split("/")[2];
+//     res.writeHead(200, { "content-type": "text/plain" });
+//     res.end(`User Id:- ${userId}`);
+//   } else {
+//     res.writeHead(404, { "content-type": "text/plain" });
+//     res.end("Route not found");
+//   }
+// });
+
+// Route Handler
+// const routes = {
+//   "/": (req, res) => {
+//     res.writeHead(200, { "content-type": "text/plain" });
+//     res.end("Welcome to homepage");
+//   },
+//   "/about": (req, res) => {
+//     res.writeHead(200, { "content-type": "text/plain" });
+//     res.end("Welcome to About Page");
+//   },
+//   "/notfound": (req, res) => {
+//     res.writeHead(404, { "content-type": "text/plain" });
+//     res.end("Page not found");
+//   },
+// };
+
+// const server = http.createServer((req, res) => {
+//   const { pathname } = url.parse(req.url);
+//   if (routes[pathname]) {
+//     routes[pathname](req, res);
+//   } else {
+//     routes["/notfound"](req, res);
+//   }
+// });
 
 // Get method
 // const server = http.createServer((req, res) => {
@@ -30,22 +108,22 @@ const url = require("url");
 // });
 
 // handle query
-const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url.startsWith("/search")) {
-    // it contains all the query from the url
-    const queryObject = url.parse(req.url, true).query;
+// const server = http.createServer((req, res) => {
+//   if (req.method === "GET" && req.url.startsWith("/search")) {
+//     // it contains all the query from the url
+//     const queryObject = url.parse(req.url, true).query;
 
-    res.writeHead(200, {
-      "content-type": "application/json",
-      "custom-header": "Node JS server",
-      "custom-trackin": "1234",
-    }); 
-    res.end(JSON.stringify({ message: "Query Received", queryObject }));
-  } else {
-    res.writeHead(404, { "content-type": "text/plain" });
-    res.end("Route not found");
-  }
-});
+//     res.writeHead(200, {
+//       "content-type": "application/json",
+//       "custom-header": "Node JS server",
+//       "custom-trackin": "1234",
+//     });
+//     res.end(JSON.stringify({ message: "Query Received", queryObject }));
+//   } else {
+//     res.writeHead(404, { "content-type": "text/plain" });
+//     res.end("Route not found");
+//   }
+// });
 
 const PORT = 3000;
 
